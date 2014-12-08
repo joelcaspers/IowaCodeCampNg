4ef6cfe719bfbe5a238eb93c47bc680062040e60
Joel Caspers <caspersj@gmail.com>
2014-12-07 21:52:29 -0600

    Added basic project tasks for building and serving the project.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,26 @@
 module.exports = function(grunt) {
+
+  var allTasks = ['clean', 'copy', 'uglify'];
+
   grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      build: {
+        src: ["build"]
+      }
+    },
+    copy: {
+      src: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['**'],
+            dest: 'build'
+          }
+        ]
+      }
+    },
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -9,13 +29,39 @@ module.exports = function(grunt) {
 				src: 'src/<%= pkg.name %>.js',
 				dest: 'build/<%= pkg.name %>.min.js'
 			}
-		}
+		},
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          port: 8082,
+          base: 'build'
+        }
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: allTasks
+      },
+      html: {
+        files: '**/*.html',
+        tasks: allTasks,
+        options: {
+          livereload: true
+        }
+      }
+    }
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task(s).
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', allTasks.concat(['connect', 'watch']));
 
 };
